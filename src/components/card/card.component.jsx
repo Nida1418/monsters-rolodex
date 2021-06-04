9bf0ada4b482824b73427fc805b69562fb1cff25
Nida <nida_khan_14@yahoo.co.in>
2021-06-05 02:37:08 +1000

    adding files for github pages

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './card.styles.css';
+
+const Card = (props) => {
+	return (
+		<div className='card-container'>
+			<img
+				alt='monster'
+				src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+			></img>
+			<h2> {props.monster.name} </h2>
+			<h3>{props.monster.email}</h3>
+		</div>
+	);
+};
+
+export default Card;
